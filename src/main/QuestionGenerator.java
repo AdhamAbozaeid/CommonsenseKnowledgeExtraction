25ef469c4fd7cc2fa41c13001cb2411b2b31de9d
Adham Abozaeid <adham.abozaeid@gmail.com>
2018-11-21 21:42:57 -0700

    Added parameters as members to the class
    Signed-off-by: Adham Abozaeid <adham.abozaeid@gmail.com>

--- src/main/QuestionGenerator.java
+++ src/main/QuestionGenerator.java
@@ -1,4 +1,4 @@
-package methods;
+package main;
 
 import methods.SentenceParser;
 import module.graph.helper.GraphPassingNode;
@@ -11,7 +11,7 @@ public class QuestionGenerator {
 		RECEIPENT,
 	};
 	
-	private String Y1;
+	protected String Y1;
 	private String X;
 	private String sentence;
 	private String verb1;
@@ -20,14 +20,19 @@ public class QuestionGenerator {
 	private int x2_index;
 	private int y1_index;
 	private int y2_index;
+	private int verb1_index;
+	private int verb2_index;
 	private relation x1_relation;
 	private relation x2_relation;
 	private relation y_relation;
 	private GraphPassingNode gpn;
 	
 	public void generateQuestions() {
+		int i ,num_records=1;
+
 		retrieveKonwledgeRecords();
 		//foreach record in records{
+		for(i=0; i<num_records; i++) {
 			SentenceParser sp = SentenceParser.getInstance();
 			
 			// Extract verb1, verb2, x1_relation, x2_relation
@@ -45,12 +50,26 @@ public class QuestionGenerator {
 				y1_index = -1;
 			
 			//Needs x1_index, x2_index, x1_relation, x2_relation, y1_index, y1_relation
+			//Adham
 			generateSentence(); //Generate Sentence and Question. 
-		//}
+		}
 	}
 	
 	public static void main(String[] args) {
 			QuestionGenerator qg = new QuestionGenerator();
+			qg.sentence = "Mike was arrested by Paul becuase Mike killed Jan";
+			qg.verb1 = "arrest";
+			qg.verb2 = "kill";
+			qg.Y1 = "Paul";
+			qg.X = "Mike";
+			qg.x1_index = 0;
+			qg.x2_index = 6;
+			qg.y1_index = 4;
+			qg.verb1_index = 2;
+			qg.verb2_index = 7;			
+			qg.x1_relation = relation.RECEIPENT;
+			qg.x2_relation = relation.AGENT;
+			qg.y_relation = relation.AGENT;
 			qg.generateQuestions();
 	}
 
@@ -60,7 +79,7 @@ public class QuestionGenerator {
 	}
 	private static boolean isPerson(String entity, GraphPassingNode gpn) {
 		// TODO Auto-generated method stub
-		
+		return false;
 	}
 	
 	private static void extractSemanticRelation() {
