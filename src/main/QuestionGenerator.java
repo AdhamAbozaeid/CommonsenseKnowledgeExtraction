73ac9d9400f34eb8d0819a8ccbfac37a8647f359
Adham Abozaeid <adham.abozaeid@gmail.com>
2018-11-25 15:47:58 -0700

    Cleanup
    * Adjusted indentation
    * Commented out unused variables for now
    * Fixed minor compilation issues
    
    Signed-off-by: Adham Abozaeid <adham.abozaeid@gmail.com>

--- src/main/QuestionGenerator.java
+++ src/main/QuestionGenerator.java
@@ -2,19 +2,11 @@ package main;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 import methods.SentenceParser;
 import module.graph.helper.GraphPassingNode;
 
 public class QuestionGenerator {
-	
-	private enum relation {
-		NONE,
-		AGENT,
-		RECIPIENT,
-	};
-	
 	private String Y1;
 	private String Y2;
 	private String X;
@@ -26,8 +18,8 @@ public class QuestionGenerator {
 	private int x2_index;
 	private int y1_index;
 	private int y2_index;
-	private int connective_index;
-	private int verb1_index;
+	//private int connective_index;
+	//private int verb1_index;
 	private int verb2_index;
 	private String x1_relation;
 	private String x2_relation;
@@ -38,23 +30,22 @@ public class QuestionGenerator {
 	public boolean processKnowledge() {
 		try {
 			SentenceParser sp = SentenceParser.getInstance();
-			
+
 			// Extract verb1, verb2, x1_relation, x2_relation
-			extractKnowledge(gpn); 
-			
+			extractKnowledge(); 
+
 			// Run parser on example sentence,
 			gpn = sp.parse(sentence);
 			// get x1_index, x2_index, y1, y1_index (agent or recp according to x1_relation)
 			// Needs semantic_graph, verb1, verb2, x1_relation, x2_relation
-			extractSemanticRelation();
-			
+			extractSemanticRelation(gpn);
+
 			if (!isPerson(X, gpn, x1_index))
 				return false;
 			if (!isPerson(Y1, gpn, y1_index))
 				y1_index = -1;
-			
+
 			//Needs x1_index, x2_index, x1_relation, x2_relation, y1_index, y1_relation
-			//Adham
 			generateSentence(); //Generate Sentence and Question. 
 
 		} catch (Exception e) {
@@ -65,77 +56,77 @@ public class QuestionGenerator {
 		}
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
-			QuestionGenerator qg = new QuestionGenerator();
-			String[] records = {""};
-			// Example 1
-			qg.sentence = "Mike was arrested by Paul because Mike killed Jan";
-			qg.verb1 = "arrest";
-			qg.verb2 = "kill";
-			qg.Y1 = "paul";
-			qg.X = "mike";
-			qg.x1_index = 0;
-			qg.x2_index = 6;
-			qg.y1_index = 4;
-			qg.connective_index = 5;
-			qg.verb1_index = 2;
-			qg.verb2_index = 7;			
-			qg.x1_relation = "recipient";//relation.RECIPIENT;
-			qg.x2_relation = "agent";//relation.AGENT;
-			qg.y1_relation = "agent";//relation.AGENT;
-			qg.y2_relation = "agent";//relation.AGENT;
+		QuestionGenerator qg = new QuestionGenerator();
+		String[] records = {""};
+		// Example 1
+		qg.sentence = "Mike was arrested by Paul because Mike killed Jan";
+		qg.verb1 = "arrest";
+		qg.verb2 = "kill";
+		qg.Y1 = "paul";
+		qg.X = "mike";
+		qg.x1_index = 0;
+		qg.x2_index = 6;
+		qg.y1_index = 4;
+		//qg.connective_index = 5;
+		//qg.verb1_index = 2;
+		qg.verb2_index = 7;			
+		qg.x1_relation = "recipient";//relation.RECIPIENT;
+		qg.x2_relation = "agent";//relation.AGENT;
+		qg.y1_relation = "agent";//relation.AGENT;
+		qg.y2_relation = "agent";//relation.AGENT;
+		if(qg.processKnowledge()) {
+			System.out.println("* "+ qg.sentence);
+			System.out.println("Q: "+ qg.question);
+		}
+
+		// Example 2
+		qg.sentence = "Mike was arrested by Paul because Mike was caught by Jan";
+		qg.verb1 = "arrest";
+		qg.verb2 = "catch";
+		qg.Y1 = "paul";
+		qg.X = "mike";
+		qg.x1_index = 0;
+		qg.x2_index = 6;
+		qg.y1_index = 4;
+		//qg.connective_index = 5;
+		//qg.verb1_index = 2;
+		qg.verb2_index = 8;			
+		qg.x1_relation = "recipient";//relation.RECIPIENT;
+		qg.x2_relation = "agent";//relation.AGENT;
+		qg.y1_relation = "agent";//relation.AGENT;
+		qg.y2_relation = "agent";//relation.AGENT;
+		if(qg.processKnowledge()) {
+			System.out.println("* "+ qg.sentence);
+			System.out.println("Q: "+ qg.question);
+		}
+
+		// Example 3
+		qg.sentence = "Jon needs to think about that some more because Jon usually likes to tweak them before sending";
+		qg.verb1 = "think";
+		qg.verb2 = "tweak";
+		//qg.Y1 = null;
+		qg.X = "jon";
+		qg.x1_index = 0;
+		qg.x2_index = 9;
+		qg.y1_index = -1;
+		//qg.connective_index = 8;
+		//qg.verb1_index = 3;
+		qg.verb2_index = 13;			
+		qg.x1_relation = "recipient";
+		qg.x2_relation = "recipient";
+		qg.y1_relation = "agent";
+		qg.y2_relation = "agent";
+
+		qg.retrieveKonwledgeRecords();
+		//foreach record in records{
+		for (String record : records) {
 			if(qg.processKnowledge()) {
 				System.out.println("* "+ qg.sentence);
 				System.out.println("Q: "+ qg.question);
 			}
-			
-			// Example 2
-			qg.sentence = "Mike was arrested by Paul because Mike was caught by Jan";
-			qg.verb1 = "arrest";
-			qg.verb2 = "catch";
-			qg.Y1 = "paul";
-			qg.X = "mike";
-			qg.x1_index = 0;
-			qg.x2_index = 6;
-			qg.y1_index = 4;
-			qg.connective_index = 5;
-			qg.verb1_index = 2;
-			qg.verb2_index = 8;			
-			qg.x1_relation = "recipient";//relation.RECIPIENT;
-			qg.x2_relation = "agent";//relation.AGENT;
-			qg.y1_relation = "agent";//relation.AGENT;
-			qg.y2_relation = "agent";//relation.AGENT;
-			if(qg.processKnowledge()) {
-				System.out.println("* "+ qg.sentence);
-				System.out.println("Q: "+ qg.question);
-			}
-			
-			// Example 3
-			qg.sentence = "Jon needs to think about that some more because Jon usually likes to tweak them before sending";
-			qg.verb1 = "think";
-			qg.verb2 = "tweak";
-			qg.Y1 = "";
-			qg.X = "jon";
-			qg.x1_index = 0;
-			qg.x2_index = 9;
-			qg.y1_index = -1;
-			qg.connective_index = 8;
-			qg.verb1_index = 3;
-			qg.verb2_index = 13;			
-			qg.x1_relation = "recipient";//relation.RECIPIENT;
-			qg.x2_relation = "recipient";//relation.RECIPIENT;
-			qg.y1_relation = "agent";//relation.AGENT;
-			qg.y2_relation = "agent";//relation.AGENT;
-			
-			qg.retrieveKonwledgeRecords();
-			//foreach record in records{
-			for (String record : records) {
-				if(qg.processKnowledge()) {
-					System.out.println("* "+ qg.sentence);
-					System.out.println("Q: "+ qg.question);
-				}
-			}
+		}
 	}
 
 	private void generateSentence() throws Exception {
@@ -143,10 +134,10 @@ public class QuestionGenerator {
 		String[] tokens = sentence.split(" ");
 		tokens[x1_index] = "Tom";
 		tokens[x2_index] = "he";
-		
-		if(y_relation != null && y1_index != -1)
+
+		if(y1_relation != null && y1_index != -1)
 			tokens[y1_index] = "John";
-		
+
 		sentence = "";
 		for(i=0; i<tokens.length; i++)
 			sentence += " "+tokens[i];
@@ -168,9 +159,9 @@ public class QuestionGenerator {
 		HashMap<String,ArrayList<String>> wordSenseMap = gpn.getWordSenseMap();
 		ArrayList<String> valueList;
 		index +=1;
-//		for(String s : gpn.getAspGraph()){
-//			System.out.println(s);
-//		}
+		//		for(String s : gpn.getAspGraph()){
+		//			System.out.println(s);
+		//		}
 		valueList = wordSenseMap.get(entity+"_"+index);
 		if(valueList == null)
 			return false;
@@ -178,7 +169,7 @@ public class QuestionGenerator {
 			return false;
 		return valueList.get(1).contains("person");
 	}
-	
+
 	private void extractSemanticRelation(GraphPassingNode gpn) {
 		// TODO Auto-generated method stub
 		for(String s : gpn.getAspGraph()){
@@ -220,11 +211,11 @@ public class QuestionGenerator {
 
 	private void extractKnowledge() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void retrieveKonwledgeRecords() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
