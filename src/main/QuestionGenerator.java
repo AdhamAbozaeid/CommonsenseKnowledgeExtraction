fcaaba18d8fe57ddb637863046db59e191bbfd54
Adham Abozaeid <adham.abozaeid@gmail.com>
2018-11-26 00:42:57 -0700

    Adjusting Indentation and autoformatting
    Signed-off-by: Adham Abozaeid <adham.abozaeid@gmail.com>

--- src/main/QuestionGenerator.java
+++ src/main/QuestionGenerator.java
@@ -20,13 +20,13 @@ import org.json.JSONObject;
 
 public class QuestionGenerator {
 	// Need to declare the variables that Ananta is using in extractKnowledge below
-	
+
 	private String Y1, Y2, X, sentence, question, verb1, verb2, x1_relation, x2_relation, y1_relation, y2_relation;
 	private int x1_index, x2_index, y1_index, y2_index, verb2_index;
 	private boolean s1hasagent, s1hasrecp, s2hasagent, s2hasrecp;
 	private GraphPassingNode gpn;
 	private String[] split;
-	
+
 	public boolean processKnowledge(Document doc) {
 		try {
 			SentenceParser sp = SentenceParser.getInstance();
@@ -40,26 +40,18 @@ public class QuestionGenerator {
 			// Needs semantic_graph, verb1, verb2, x1_relation, x2_relation
 			extractSemanticRelation(gpn);
 			/*
-			System.out.println(X);
-			System.out.println(x1_index);
-			System.out.println(X);
-			System.out.println(x2_index);
-			if(Y1 != "") {
-				System.out.println(Y1);
-				System.out.println(y1_index);
-			}
-			if(Y2 != "") {
-				System.out.println(Y2);
-				System.out.println(y2_index);	
-			}
-*/
+			 * System.out.println(X); System.out.println(x1_index); System.out.println(X);
+			 * System.out.println(x2_index); if(Y1 != "") { System.out.println(Y1);
+			 * System.out.println(y1_index); } if(Y2 != "") { System.out.println(Y2);
+			 * System.out.println(y2_index); }
+			 */
 			if (!isPerson(X, gpn, x1_index))
 				return false;
 			if (!isPerson(Y1, gpn, y1_index))
 				y1_index = -1;
 
-			//Needs x1_index, x2_index, x1_relation, x2_relation, y1_index, y1_relation
-			modifySentence(); //Generate Sentence and Question. 
+			// Needs x1_index, x2_index, x1_relation, x2_relation, y1_index, y1_relation
+			modifySentence(); // Generate Sentence and Question.
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -69,7 +61,7 @@ public class QuestionGenerator {
 		}
 		return true;
 	}
-	
+
 	private void extractKnowledge(Document doc) throws JSONException {
 		JSONObject d = new JSONObject(doc.toJson());
 		s1hasagent = d.getJSONObject("knowledge").has("agent");
@@ -79,16 +71,13 @@ public class QuestionGenerator {
 		s2hasrecp = d.getJSONObject("knowledge").getJSONObject("causes").has("recipient");
 		verb2 = d.getJSONObject("knowledge").getJSONObject("causes").getString("value");
 		sentence = (String) d.getJSONArray("texts").get(0);
-		if(s1hasagent == true) {
+		if (s1hasagent == true) {
 			x1_relation = "agent";
-		}
-		else if(s1hasrecp == true) {
+		} else if (s1hasrecp == true) {
 			x1_relation = "recipient";
-		}
-		else if(s2hasagent == true) {
+		} else if (s2hasagent == true) {
 			x2_relation = "agent";
-		}
-		else if(s1hasrecp == true) {
+		} else if (s1hasrecp == true) {
 			x2_relation = "recipient";
 		}
 	}
@@ -99,105 +88,100 @@ public class QuestionGenerator {
 		tokens[x1_index] = "Tom";
 		tokens[x2_index] = "he";
 
-		if(y1_relation != null && y1_index != -1)
+		if (y1_relation != null && y1_index != -1)
 			tokens[y1_index] = "John";
 
 		sentence = "";
-		for(i=0; i<tokens.length; i++)
-			sentence += " "+tokens[i];
+		for (i = 0; i < tokens.length; i++)
+			sentence += " " + tokens[i];
 		// Build question
-		if(x2_relation.equals("agent")) {
+		if (x2_relation.equals("agent")) {
 			question = "who";
-		} else if(x2_relation.equals("recipient")) {
+		} else if (x2_relation.equals("recipient")) {
 			question = "who was";
 		} else {
 			throw new Exception("Unknown X relation");
 		}
-		for(i=verb2_index; i<tokens.length; i++) {
-			question += " "+tokens[i];
+		for (i = verb2_index; i < tokens.length; i++) {
+			question += " " + tokens[i];
 		}
 		question += "?";
 	}
 
 	private boolean isPerson(String entity, GraphPassingNode gpn, int index) {
-		HashMap<String,ArrayList<String>> wordSenseMap = gpn.getWordSenseMap();
+		HashMap<String, ArrayList<String>> wordSenseMap = gpn.getWordSenseMap();
 		ArrayList<String> valueList;
-		index +=1;
-		//		for(String s : gpn.getAspGraph()){
-		//			System.out.println(s);
-		//		}
-		valueList = wordSenseMap.get(entity+"_"+index);
-		if(valueList == null)
+		index += 1;
+		// for(String s : gpn.getAspGraph()){
+		// System.out.println(s);
+		// }
+		valueList = wordSenseMap.get(entity + "_" + index);
+		if (valueList == null)
 			return false;
-		if(valueList.get(1) == null)
+		if (valueList.get(1) == null)
 			return false;
 		return valueList.get(1).contains("person");
 	}
 
 	private void extractSemanticRelation(GraphPassingNode gpn) {
 		// TODO Auto-generated method stub
-		for(String s : gpn.getAspGraph()){
-			if(s.contains(verb1) && s.contains(x1_relation)) {
+		for (String s : gpn.getAspGraph()) {
+			if (s.contains(verb1) && s.contains(x1_relation)) {
 				split = s.split("\\D+");
-				x1_index = Integer.parseInt(split[split.length-1])-1;
-				//System.out.println(X);
-				//System.out.println(x1_index);
-			}
-			else if(s.contains(verb2) && s.contains(x2_relation)) {
+				x1_index = Integer.parseInt(split[split.length - 1]) - 1;
+				// System.out.println(X);
+				// System.out.println(x1_index);
+			} else if (s.contains(verb2) && s.contains(x2_relation)) {
 				split = s.split("\\D+");
-				x2_index = Integer.parseInt(split[split.length-1])-1;
-				verb2_index = Integer.parseInt(split[split.length-2])-1;
-				//System.out.println(verb2_index);
-			}
-			else if(s.contains(verb1) && s.contains("agent") || s.contains(verb1) && s.contains("recipient")) {
+				x2_index = Integer.parseInt(split[split.length - 1]) - 1;
+				verb2_index = Integer.parseInt(split[split.length - 2]) - 1;
+				// System.out.println(verb2_index);
+			} else if (s.contains(verb1) && s.contains("agent") || s.contains(verb1) && s.contains("recipient")) {
 				split = s.split(",");
-				split = split[split.length-1].split("-");
-				if(!split[0].equals(X)) {
+				split = split[split.length - 1].split("-");
+				if (!split[0].equals(X)) {
 					Y1 = split[0].toLowerCase();
-					if(s.contains("agent")) {
+					if (s.contains("agent")) {
 						y1_relation = "agent";
-					}
-					else {
+					} else {
 						y1_relation = "recipient";
 					}
 				}
 				split = s.split("\\D+");
-				y1_index = Integer.parseInt(split[split.length-1])-1;
-				//System.out.println(Y1);
-				//System.out.println(y1_index);
-			}
-			else if(s.contains(verb2) && s.contains("agent") || s.contains(verb2) && s.contains("recipient")) {
+				y1_index = Integer.parseInt(split[split.length - 1]) - 1;
+				// System.out.println(Y1);
+				// System.out.println(y1_index);
+			} else if (s.contains(verb2) && s.contains("agent") || s.contains(verb2) && s.contains("recipient")) {
 				split = s.split(",");
-				split = split[split.length-1].split("-");
-				if(!split[0].equals(X)) {
+				split = split[split.length - 1].split("-");
+				if (!split[0].equals(X)) {
 					Y2 = split[0].toLowerCase();
-					if(s.contains("agent")) {
+					if (s.contains("agent")) {
 						y2_relation = "agent";
-					}
-					else {
+					} else {
 						y2_relation = "recipient";
 					}
 				}
 				split = s.split("\\D+");
-				y2_index = Integer.parseInt(split[split.length-1])-1;
-				//System.out.println(Y1);
-				//System.out.println(y1_index);
+				y2_index = Integer.parseInt(split[split.length - 1]) - 1;
+				// System.out.println(Y1);
+				// System.out.println(y1_index);
 			}
 
 		}
-		
+
 	}
-	
-		public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		QuestionGenerator qg = new QuestionGenerator();
-		//testExamples();
+		// testExamples();
 		try {
 			FindIterable<Document> docs = qg.retrieveKonwledgeRecords();
-		
+
 			for (Document doc : docs) {
-				if(qg.processKnowledge(doc)) {
-					System.out.println("* "+ qg.sentence);
-					System.out.println("Q: "+ qg.question);
+				if (qg.processKnowledge(doc)) {
+					System.out.println("* " + qg.sentence);
+					System.out.println("Q: " + qg.question);
 				}
 			}
 		} catch (JSONException e) {
@@ -207,7 +191,7 @@ public class QuestionGenerator {
 
 	private FindIterable<Document> retrieveKonwledgeRecords() throws JSONException {
 		QuestionGenerator qg = new QuestionGenerator();
-		
+
 		int port_no = 27017;
 		String host_name = "localhost", db_name = "knetdb", db_coll_name = "maintable";
 		String client_url = "mongodb://" + host_name + ":" + port_no + "/" + db_name;
@@ -220,72 +204,73 @@ public class QuestionGenerator {
 		return docs;
 
 	}
-	
-	public FindIterable<Document> findKnowledgeInKB(MongoCollection<Document> col){
+
+	public FindIterable<Document> findKnowledgeInKB(MongoCollection<Document> col) {
 		FindIterable<Document> docs = col.find(Filters.eq("type", "action causes action"));
 		return docs;
 	}
 
 	@SuppressWarnings("unused")
 	private void testExamples() {
-		//		String[] records = {""};
-		//		// Example 1
-		//		qg.sentence = "Mike was arrested by Paul because Mike killed Jan";
-		//		qg.verb1 = "arrest";
-		//		qg.verb2 = "kill";
-		//		//qg.Y1 = "paul";
-		//		qg.X = "mike";
-		//		//qg.x1_index = 0;
-		//		//qg.x2_index = 6;
-		//		//qg.y1_index = 4;
-		//		//qg.connective_index = 5;
-		//		//qg.verb1_index = 2;
-		//		//qg.verb2_index = 7;			
-		//		qg.x1_relation = "recipient";//relation.RECIPIENT;
-		//		qg.x2_relation = "agent";//relation.AGENT;
-		//		//qg.y1_relation = "agent";//relation.AGENT;
-		//		//qg.y2_relation = "agent";//relation.AGENT;
-		//		if(qg.processKnowledge()) {
-		//			System.out.println("* "+ qg.sentence);
-		//			System.out.println("Q: "+ qg.question);
-		//		}
+		// String[] records = {""};
+		// // Example 1
+		// qg.sentence = "Mike was arrested by Paul because Mike killed Jan";
+		// qg.verb1 = "arrest";
+		// qg.verb2 = "kill";
+		// //qg.Y1 = "paul";
+		// qg.X = "mike";
+		// //qg.x1_index = 0;
+		// //qg.x2_index = 6;
+		// //qg.y1_index = 4;
+		// //qg.connective_index = 5;
+		// //qg.verb1_index = 2;
+		// //qg.verb2_index = 7;
+		// qg.x1_relation = "recipient";//relation.RECIPIENT;
+		// qg.x2_relation = "agent";//relation.AGENT;
+		// //qg.y1_relation = "agent";//relation.AGENT;
+		// //qg.y2_relation = "agent";//relation.AGENT;
+		// if(qg.processKnowledge()) {
+		// System.out.println("* "+ qg.sentence);
+		// System.out.println("Q: "+ qg.question);
+		// }
 		//
-		//		// Example 2
-		//		qg.sentence = "Mike was arrested by Paul because Mike was caught by Jan";
-		//		qg.verb1 = "arrest";
-		//		qg.verb2 = "catch";
-		//		//qg.Y1 = "paul";
-		//		qg.X = "mike";
-		//		//qg.x1_index = 0;
-		//		//qg.x2_index = 6;
-		//		//qg.y1_index = 4;
-		//		//qg.connective_index = 5;
-		//		//qg.verb1_index = 2;
-		//		//qg.verb2_index = 8;			
-		//		qg.x1_relation = "recipient";//relation.RECIPIENT;
-		//		qg.x2_relation = "recipient";//relation.AGENT;
-		//		//qg.y1_relation = "agent";//relation.AGENT;
-		//		//qg.y2_relation = "agent";//relation.AGENT;
-		//		if(qg.processKnowledge()) {
-		//			System.out.println("* "+ qg.sentence);
-		//			System.out.println("Q: "+ qg.question);
-		//		}
+		// // Example 2
+		// qg.sentence = "Mike was arrested by Paul because Mike was caught by Jan";
+		// qg.verb1 = "arrest";
+		// qg.verb2 = "catch";
+		// //qg.Y1 = "paul";
+		// qg.X = "mike";
+		// //qg.x1_index = 0;
+		// //qg.x2_index = 6;
+		// //qg.y1_index = 4;
+		// //qg.connective_index = 5;
+		// //qg.verb1_index = 2;
+		// //qg.verb2_index = 8;
+		// qg.x1_relation = "recipient";//relation.RECIPIENT;
+		// qg.x2_relation = "recipient";//relation.AGENT;
+		// //qg.y1_relation = "agent";//relation.AGENT;
+		// //qg.y2_relation = "agent";//relation.AGENT;
+		// if(qg.processKnowledge()) {
+		// System.out.println("* "+ qg.sentence);
+		// System.out.println("Q: "+ qg.question);
+		// }
 		//
-		//		// Example 3
-		//		qg.sentence = "Jon needs to think about that some more because Jon usually likes to tweak them before sending";
-		//		qg.verb1 = "think";
-		//		qg.verb2 = "tweak";
-		//		//qg.Y1 = null;
-		//		qg.X = "jon";
-		//		//qg.x1_index = 0;
-		//		//qg.x2_index = 9;
-		//		//qg.y1_index = -1;
-		//		//qg.connective_index = 8;
-		//		//qg.verb1_index = 3;
-		//		//qg.verb2_index = 13;			
-		//		qg.x1_relation = "recipient";
-		//		qg.x2_relation = "recipient";
-		//		//qg.y1_relation = "agent";
-		//		//qg.y2_relation = "agent";
+		// // Example 3
+		// qg.sentence = "Jon needs to think about that some more because Jon usually
+		// likes to tweak them before sending";
+		// qg.verb1 = "think";
+		// qg.verb2 = "tweak";
+		// //qg.Y1 = null;
+		// qg.X = "jon";
+		// //qg.x1_index = 0;
+		// //qg.x2_index = 9;
+		// //qg.y1_index = -1;
+		// //qg.connective_index = 8;
+		// //qg.verb1_index = 3;
+		// //qg.verb2_index = 13;
+		// qg.x1_relation = "recipient";
+		// qg.x2_relation = "recipient";
+		// //qg.y1_relation = "agent";
+		// //qg.y2_relation = "agent";
 	}
 }
